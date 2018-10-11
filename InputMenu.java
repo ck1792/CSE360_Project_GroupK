86c19688fb66ce2f5e21681050e4effd722c1c18
ck1792 <ckchen1792@gmail.com>
2018-10-10 18:48:31 -0700

    Update InputMenu.java

--- InputMenu.java
+++ InputMenu.java
@@ -4,18 +4,24 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
 public class InputMenu extends JFrame{
+	
+	private Input_node head;
+	private int counter = 0;
+	private String print = "";
 
 	private JFrame frame;
 	private JTextField activity;
 	private JTextField dependencies;
 	private JTextField duration;
 	private JTextField output;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -46,17 +52,17 @@ public class InputMenu extends JFrame{
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Process");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				OutputWindow nw = new OutputWindow();
-				nw.OutputScreen();
-				frame.setVisible(false);
-				frame.dispose();
+//				OutputWindow nw = new OutputWindow();
+//				nw.OutputScreen();
+//				frame.setVisible(false);
+//				frame.dispose();
 			}
 		});
 		btnNewButton.setBounds(98, 232, 88, 29);
@@ -99,6 +105,8 @@ public class InputMenu extends JFrame{
 				activity.setText("");
 				dependencies.setText("");
 				duration.setText("");
+				head = null;
+				counter = 0;
 			}
 		});
 		btnRestart.setBounds(366, 232, 78, 29);
@@ -123,11 +131,81 @@ public class InputMenu extends JFrame{
 		output.setColumns(10);
 		
 		JButton btnAdd = new JButton("Add Node");
+		//
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = activity.getText();
+				String b = dependencies.getText();
+				int c = 0;
+				try{
+					c = Integer.parseInt(duration.getText());
+					Input_node node = new Input_node(a,b,c);
+					counter += 1;
+					if(head == null){
+						head = node;
+					}
+					else{
+						Input_node temp = head;
+						while(true){
+							if(temp.get_next() == null){
+								break;
+							}
+							temp = temp.get_next();
+						}
+						temp.set_next(node);
+					}
+					activity.setText("");
+					dependencies.setText("");
+					duration.setText("");
+					output.setText("Please enter next values.");
+				} catch(NumberFormatException exception){
+					//fails
+					output.setText("Duration must be integer.");
+				}
+			}
+		});
+		//
 		btnAdd.setBounds(6, 232, 88, 29);
 		frame.getContentPane().add(btnAdd);
 		
 		JLabel lblOutput = new JLabel("Output");
 		lblOutput.setBounds(63, 166, 61, 16);
 		frame.getContentPane().add(lblOutput);
+		
+		JLabel lblOutput2 = new JLabel("Output");
+		lblOutput2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOutput2.setBounds(186, 320, 61, 16);
+		frame.getContentPane().add(lblOutput2);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(50, 349, 348, 134);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton output = new JButton("Get output");
+		output.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(get_print());
+			}
+		});
+		output.setBounds(182, 495, 100, 29);
+		frame.getContentPane().add(output);
+	}
+	
+	public String get_print(){
+		Input_node temp2 = head;
+		print = "";
+		for(int i = 0; i < counter; i++){
+//			System.out.println(temp2.get_activity());
+//			System.out.println(temp2.get_dependency());
+//			System.out.println(temp2.get_duration());
+			print = print + temp2.get_activity() + " ";
+			print = print + temp2.get_dependency() + " ";
+			print = print + temp2.get_duration() + " ";
+			temp2 = temp2.get_next();
+		}
+//		System.out.println(print);
+		return print;
 	}
 }
