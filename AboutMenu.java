6855e34c36e9925c15b10398730fa24e2532d2a4
Dekart Kosa <dekartkosa1997@gmail.com>
2018-10-10 17:08:07 -0700

    Update AboutMenu.java

--- AboutMenu.java
+++ AboutMenu.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AboutMenu {
 
@@ -37,22 +41,38 @@ public class AboutMenu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JTextArea txtrProjectTheProgram = new JTextArea();
+		txtrProjectTheProgram.setEditable(false);
 		txtrProjectTheProgram.setText("Project Overview: The program is to analyze a network diagram \nand determine all the paths in the netwrok");
-		txtrProjectTheProgram.setBounds(6, 109, 438, 39);
+		txtrProjectTheProgram.setBounds(6, 70, 438, 39);
 		frame.getContentPane().add(txtrProjectTheProgram);
 		
 		JTextArea txtrTeamMembersDekart = new JTextArea();
+		txtrTeamMembersDekart.setBackground(Color.WHITE);
+		txtrTeamMembersDekart.setEditable(false);
 		txtrTeamMembersDekart.setText("Team Members: Dekart Kosa, Xiang Li, Jiwon Kim, Kai Chen");
-		txtrTeamMembersDekart.setBounds(6, 160, 438, 21);
+		txtrTeamMembersDekart.setBounds(6, 121, 438, 21);
 		frame.getContentPane().add(txtrTeamMembersDekart);
 		
 		JLabel lblAboutMenu = new JLabel("About Menu");
 		lblAboutMenu.setBounds(186, 37, 76, 21);
 		frame.getContentPane().add(lblAboutMenu);
+		
+		JButton btnReturnToMain = new JButton("Return to Main Menu");
+		btnReturnToMain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainMenu nw = new MainMenu();
+				nw.main(null);
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+		btnReturnToMain.setBounds(151, 211, 174, 29);
+		frame.getContentPane().add(btnReturnToMain);
 	}
 }
