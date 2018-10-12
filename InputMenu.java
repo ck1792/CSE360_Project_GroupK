70cc450bc5963f190012c931be86262a79b3e702
ck1792 <ckchen1792@gmail.com>
2018-10-12 00:25:57 -0700

    Update InputMenu.java

--- InputMenu.java
+++ InputMenu.java
@@ -7,6 +7,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 
@@ -15,14 +17,15 @@ public class InputMenu extends JFrame{
 	private Input_node head;
 	private int counter = 0;
 	private String print = "";
+	private int error_message = 0;
+	private List<String> dependencies_list;
 
 	private JFrame frame;
 	private JTextField activity;
 	private JTextField dependencies;
 	private JTextField duration;
 	private JTextField output;
-	private JTextField textField;
-	private int error_message = 0;
+	private JTextArea textField;
 	/**
 	 * Launch the application.
 	 */
@@ -54,14 +57,7 @@ public class InputMenu extends JFrame{
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 450, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-//				OutputWindow nw = new OutputWindow();
-//				nw.OutputScreen();
-//				frame.setVisible(false);
-//				frame.dispose();
-			
-		
+		frame.getContentPane().setLayout(null);	
 		
 		JLabel lblInputMenu = new JLabel("Input Menu");
 		lblInputMenu.setBounds(186, 16, 71, 16);
@@ -130,32 +126,42 @@ public class InputMenu extends JFrame{
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String a = activity.getText();
-				String b = dependencies.getText();
-				int c = 0;
-				try{
-					c = Integer.parseInt(duration.getText());
-					Input_node node = new Input_node(a,b,c);
-					counter += 1;
-					if(head == null){
-						head = node;
-					}
-					else{
-						Input_node temp = head;
-						while(true){
-							if(temp.get_next() == null){
-								break;
-							}
-							temp = temp.get_next();
+				if(a.equals("")){
+					output.setText("Please input activity.");
+				}
+				else{
+					String b = dependencies.getText();
+					int c = 0;
+					try{
+						c = Integer.parseInt(duration.getText());
+						if(c == 0){
+							output.setText("Duration can not be 0.");
 						}
-						temp.set_next(node);
+						else{
+							Input_node node = new Input_node(a,b,c);
+							counter += 1;
+							if(head == null){
+								head = node;
+							}
+							else{
+								Input_node temp = head;
+								while(true){
+									if(temp.get_next() == null){
+										break;
+									}
+									temp = temp.get_next();
+								}
+								temp.set_next(node);
+							}
+							activity.setText("");
+							dependencies.setText("");
+							duration.setText("");
+							output.setText("A new node has been added.");
+						}
+					} catch(NumberFormatException exception){
+						//fails
+						output.setText("Duration must be integer.");
 					}
-					activity.setText("");
-					dependencies.setText("");
-					duration.setText("");
-					output.setText("Please enter next values.");
-				} catch(NumberFormatException exception){
-					//fails
-					output.setText("Duration must be integer.");
 				}
 			}
 		});
@@ -172,7 +178,7 @@ public class InputMenu extends JFrame{
 		lblOutput2.setBounds(186, 320, 61, 16);
 		frame.getContentPane().add(lblOutput2);
 		
-		textField = new JTextField();
+		textField = new JTextArea();
 		textField.setEditable(false);
 		textField.setBounds(50, 349, 348, 134);
 		frame.getContentPane().add(textField);
@@ -181,48 +187,52 @@ public class InputMenu extends JFrame{
 		JButton output = new JButton("Process");
 		output.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textField.setText(get_print());
+				textField.setText("");
+				textField.append(print());
 			}
 		});
 		output.setBounds(182, 495, 100, 29);
 		frame.getContentPane().add(output);
 	}
-	//
+
+	//error check
+	//connection check
 	public void matcherror() {
-		String testing = "";
+		int end_counter = 0;
 		Input_node temp3 = head;
-	for(int i = 0; i < counter; i++) {
-		boolean match = false;
-		testing = temp3.get_activity();
-		if(temp3.get_dependency().equals(testing)) {
-			Input_node temp4 = head;
-		for(int j = 0; j < counter; j++) {
-			if(temp3.get_dependency() != testing){
-				if(temp3.get_dependency() == temp4.get_activity()){
-					match = true;
+		for(int i = 0; i < counter; i++) {
+			boolean match = false;
+			get_dependencies(temp3.get_dependency());
+			if(dependencies_list.get(0).equals("")){
+				end_counter += 1;
+				Input_node temp4 = head;
+				for(int j =0; j < counter; j++){
+					get_dependencies(temp4.get_dependency());
+					for(int index = 0; index < dependencies_list.size(); index++){
+						if(temp3.get_activity().equals(dependencies_list.get(index))){
+							match = true;
+						}
+					}
+					temp4 = temp4.get_next();
 				}
-				}
-			 temp4 = temp4.get_next();
 			}
+			else{
+				match = true;
+			}
+			if(match == false && end_counter > 1) {
+				error_message = 1;
+			}
+			temp3= temp3.get_next();
 		}
-		else {
-			match = true;
-		}
-		if(match == false) {
-			error_message = 1;
-		}
-		
-		temp3= temp3.get_next();
-	}
 	}
 	
+	//cycle check
 	public void cycle_check() {
 		Input_node temp = head;
 		int cyclecounter = 0;
 		for(int i =0; i < counter; i++) {
 			if(temp.get_dependency().equals("")) {
-				cyclecounter++; //will + on cycle counter to counter if no dependency is checked
-			
+				cyclecounter++;
 			}
 			temp = temp.get_next();
 		}
@@ -234,69 +244,97 @@ public class InputMenu extends JFrame{
 		}
 	}
 
+	//index by biggest duration
 	public void sort() {
-		Input_node temp = head;
-		Input_node temp2 = head;
-		for (int i = 0; i < counter; i++) {
-			int min = 9999;
-			for( int j = 0; j < counter; j++) {
-				if(temp.get_duration() < min) {
-					temp.set_index(i);
-					min = temp.get_duration();
-					temp2.set_index(999);//this will make the index set before to some other value high
+		Input_node initialize = head;
+		for(int i = 0; i < counter; i ++){
+			initialize.set_index(0);
+			initialize = initialize.get_next();
+		}
+		int index = 0;
+		while(true){
+			boolean changed = false;
+			Input_node temp = head;
+			for (int i = 0; i < counter; i++) {
+				boolean biggest = true;
+				Input_node temp2 = head;
+				for( int j = 0; j < counter; j++) {
+					if(!temp.get_activity().equals(temp2.get_activity())){
+						if(temp.get_index() == 0){
+							if(temp2.get_index() == 0){
+								if(temp.get_duration() < temp2.get_duration()){
+									biggest = false;
+								}
+							}
+						}
+					}
+					temp2 = temp2.get_next();
 				}
-				if(temp.get_duration() == min) {
-					temp2.set_index(i+1);
-					temp.set_index(i);
+				if(biggest){
+					if(temp.get_index() == 0){
+						index += 1;
+						temp.set_index(index);
+						changed = true;
+					}
 				}
-			
-				temp2 = temp;
 				temp = temp.get_next();
 			}
-		}
-		sortbyindex();//indexed every nodes, need to sort by index
-	}
-	
-	public void sortbyindex() {
-		Input_node temp = head;
-		Input_node temp2 = head;
-		for(int i = 0; i < counter; i++) {
-			for(int j = 0; j < counter; j++) {
-				if(temp.get_index() < temp.get_next().get_index()) {
-					temp2 = temp.get_next();
-					temp.set_next(temp);
-					temp = temp2;
-				}
-				temp = temp.get_next();
-				
+			if(!changed){
+				break;
 			}
 		}
-	
 	}
-	//
-	public String get_print(){
+	
+	public void get_dependencies(String str)
+	{
+		dependencies_list = Arrays.asList(str.split("\\s*,\\s*"));
+	}
+
+	//print function
+	public String print(){
 		//
+		//must have a dependency, must be mentioned in dependency
+		//error check for all nodes must be connected
+		//there cannot be a cycle
 		matcherror();
 		cycle_check();
-		sort();
 		//
-		Input_node temp2 = head;
+
+//        //this is for test only
+//		get_dependencies(head.get_dependency());
+//		for(int i = 0; i < dependencies_list.size(); i++){
+//			System.out.println(dependencies_list.get(i));
+//		}
+//
+//		Input_node test = head;
+//		for(int i = 0; i < counter; i ++){
+//			System.out.println(test.get_index());
+//			test = test.get_next();
+//		}
+
+		sort();
 		print = "";
+		int index_counter = 1;
 		for(int i = 0; i < counter; i++){
-			print = print + temp2.get_activity() + " ";
-			print = print + temp2.get_dependency() + " ";
-			print = print + temp2.get_duration() + " ";
-			temp2 = temp2.get_next();
+			Input_node temp = head;
+			for(int j = 0; j < counter; j++){
+				if(temp.get_index() == index_counter){
+					print = print + "activity: " + temp.get_activity() + " ";
+					print = print + "dependency: " + temp.get_dependency() + " ";
+					print = print + "duration: " + temp.get_duration() + " ";
+					print = print + "\n";
+				}
+				temp = temp.get_next();
+			}
+			index_counter += 1;
 		}
-		if (error_message == 1) {
-		return ("Error has occurred");
-		}else {
+		if (error_message == 1){
+			error_message = 0;
+			return ("Error has occurred");
+		}
+		else{
 			return print;
 		}
 	}
-	//must have a dependency, must be mentioned in dependency
-	//error check for all nodes must be connected
-	
-	//there cannot be a cycle
 	
 }
